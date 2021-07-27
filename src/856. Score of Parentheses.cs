ed7b40902c4aae6f15ff31e0dbe5573d0dc8ff46
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-07-26 23:50:54 -0700

    856. Score of Parentheses

---
+++ src/856. Score of Parentheses.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public int ScoreOfParentheses(string s) {
+        int res = 0, cnt = 0;
+        for (int i = 0; i < s.Length; i++) {
+            cnt += s[i] == '(' ? 1 : -1;
+            // () => 2^0, (()) => 2^1, ((())) => 2^2
+            if (s[i] == ')' && s[i-1] == '(') res += 1 << cnt;
+        }
+        // O(n)
+        return res;
+    }
+    
+    public int ScoreOfParentheses2(string s) {
+        if (s == "") return 0;
+        if (s == "()") return 1;
+        int cnt = 0;
+        for (int i = 0; i < s.Length - 1; i++) {
+            cnt += (s[i] == '(') ? 1 : -1;
+            if (cnt == 0) 
+                return ScoreOfParentheses(s.Substring(0, i - 0 + 1)) + ScoreOfParentheses(s.Substring(i+1));
+        }
+        // O(n^2)
+        return 2 * ScoreOfParentheses(s.Substring(1, s.Length - 1));
+    }
+    
+    public int ScoreOfParentheses1(string s) {
+        int i = 0;
+        return GetScore(s, ref i);
+    }
+    int GetScore(string s, ref int i) {
+        int res = 0;
+        while (i < s.Length) {
+            if (s[i] == '(') {
+                i++;
+                res += GetScore(s, ref i);
+            }
+            else { // s[i] == ')'
+                i++;
+                res = res == 0 ? 1: res * 2;
+                break;
+            }
+        }
+        return res;
+    }
+}
