92d66d6a175e3304c9c327fa47b10d74ffbb0caa
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-07-29 18:13:47 -0700

    218. The Skyline Problem

---
+++ src/218. The Skyline Problem.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public IList<IList<int>> GetSkyline(int[][] buildings) {
+        var bhLst = new List<(int, int)>();
+        foreach (int[] b in buildings) {
+            bhLst.Add((b[0], b[2]));
+            bhLst.Add((b[1], -b[2])); // mark as end
+        }
+        bhLst.Sort((x, y) => {
+            if (x.Item1 == y.Item1) return y.Item2 - x.Item2;
+            else return x.Item1 - y.Item1;
+            });
+        int prev = 0;
+        var h = new List<int>(){prev};
+        var ans = new List<IList<int>>();
+        foreach (var bh in bhLst) {
+            if (bh.Item2 > 0) InsertList(h, bh.Item2);
+            else h.Remove(-bh.Item2); // -Item2 is end
+            int cur = h.Last();
+            if (cur != prev) {
+                ans.Add(new int[]{bh.Item1, cur});
+                prev = cur;
+            }
+        }
+        return ans;
+    }
+    // List sort is too slow for LeetCode
+    // Binary search for insert
+    public void InsertList(List<int> lst, int x) {
+        int l = 0, r = lst.Count;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (lst[m] < x) l = m + 1;
+            else r = m;
+        }
+        lst.Insert(r, x);
+    }
+}
