8ebc905c934e6c83259ca7fc3bf5e369f4af5d2f
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-07-30 21:35:42 -0700

    270. Closest Binary Search Tree Value update

--- src/270. Closest Binary Search Tree Value.cs
+++ src/270. Closest Binary Search Tree Value.cs
@@ -12,13 +12,22 @@
  * }
  */
 public class Solution {
-    public int ClosestValue(TreeNode root, double target) {
+    public int ClosestValue2(TreeNode root, double target) {
         int ans = Int32.MaxValue;
         while (root != null) {
             if (Math.Abs(ans - target) >= Math.Abs(root.val - target)) ans = root.val;
             root = target < root.val ? root.left : root.right;
         }
         return ans;
+    }
+    public int ClosestValue(TreeNode root, double target) {
+        int x = root.val;
+        var t = target < x ? root.left : root.right;
+        if (t == null) return x;
+        else {
+            int y = ClosestValue(t, target);
+            return Math.Abs(x - target) < Math.Abs(y - target) ? x : y;
+        }
     }
     public int ClosestValue1(TreeNode root, double target) {
         if (root == null) return Int32.MaxValue;
