d1ea665871a81409d9b48960018f5e54e8c84203
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-01 23:42:43 -0700

    112. Path Sum

---
+++ src/112. Path Sum.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && root.val == targetSum) return true;
+        int t = targetSum - root.val;
+        return HasPathSum(root.left, t) || HasPathSum(root.right, t);
+    }
+}
