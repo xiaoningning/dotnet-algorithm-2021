0d44d0c5bb4d643e8056d1c65709fd71334decde
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-02 00:33:28 -0700

    112. Path sum update cmt

--- src/112. Path Sum.cs
+++ src/112. Path Sum.cs
@@ -13,7 +13,7 @@
  */
 public class Solution {
     public bool HasPathSum(TreeNode root, int targetSum) {
-        if (root == null) return false;
+        if (root == null) return false; // HasPathSum(null, 0) => false
         if (root.left == null && root.right == null && root.val == targetSum) return true;
         int t = targetSum - root.val;
         return HasPathSum(root.left, t) || HasPathSum(root.right, t);
