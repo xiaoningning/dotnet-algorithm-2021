6b83949990779262390e4e5758192b6f3e8ecf77
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-02 17:04:02 -0700

    979. Distribute Coins in Binary Tree

---
+++ src/979. Distribute Coins in Binary Tree.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    // Total n nodes, total n coins
+    // all moves count
+    int ans = 0;
+    public int DistributeCoins(TreeNode root) {
+        GetMoves(root);
+        return ans;
+    }
+    int GetMoves(TreeNode root) {
+        if (root == null) return 0;
+        Console.WriteLine("r " + root.val);
+        int left = GetMoves(root.left);
+        int right = GetMoves(root.right);
+        // balance left and right coins
+        ans += Math.Abs(left) + Math.Abs(right);
+        // - move in, + move out
+        // moves of subtree
+        return left + right + root.val - 1;
+    }
+}
