5a5d655d18d095908ab8ac7f26097802bdb4789a
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-02 23:57:44 -0700

    101. Symmetric Tree

---
+++ src/101. Symmetric Tree.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    // T: O(n), S: O(n)
+    public bool IsSymmetric(TreeNode root) {
+        return IsSymmetric(root, root);
+    }
+    bool IsSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return left.val == right.val 
+            && IsSymmetric(left.left, right.right)
+            && IsSymmetric(left.right, right.left);
+    }
+}
