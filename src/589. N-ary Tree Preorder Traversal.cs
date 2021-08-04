2de604edbcf3331902844664e6a3eca66362c936
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-03 22:14:42 -0700

    589. N-ary Tree Preorder Traversal

---
+++ src/589. N-ary Tree Preorder Traversal.cs
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    List<int> ans = new List<int>();
+    public IList<int> Preorder(Node root) {
+        if (root == null) return ans;
+        ans.Add(root.val);
+        foreach (Node c in root.children) Preorder(c);
+        return ans;
+    }
+}
