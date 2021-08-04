6a294bc3ab5a4447e226d1a4001ac1600f6661d4
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-04 10:33:05 -0700

    875. Koko Eating Bananas

---
+++ src/875. Koko Eating Bananas.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        if (piles.Length == 0) return 0;
+        // h >= piles.length
+        // speed can be 1  if h is big 
+        // or max itself if h is small
+        int l = 1, r = piles.Max() + 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;   
+            int cnt = 0;
+            foreach (var n in piles) cnt += (n + m - 1) / m;
+            if (cnt <= h) r = m;
+            else l = m + 1;
+            
+        }
+        return r;
+    }
+}
