56ac4fb93a9bb7bcd81690c6a4bd1d5b15829fb8
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-05 00:13:36 -0700

    Update 852. Peak Index in a Mountain Array.cs

--- src/852. Peak Index in a Mountain Array.cs
+++ src/852. Peak Index in a Mountain Array.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public int PeakIndexInMountainArray(int[] arr) {
+        // guarantee a peak and go down, never out of boarder
         int l = 0, r = arr.Length;
         while (l < r) {
             int peak = l + (r - l) / 2;
