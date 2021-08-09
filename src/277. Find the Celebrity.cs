55cbd8d55d820cc72e240588c35d16eee6ad68fd
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-09 11:52:23 -0700

    277. Find the Celebrity in/out degree of graph node

--- src/277. Find the Celebrity.cs
+++ src/277. Find the Celebrity.cs
@@ -2,7 +2,7 @@
       bool Knows(int a, int b); */
 
 public class Solution : Relation {
-    public int FindCelebrity(int n) {
+    public int FindCelebrity1(int n) {
         int ans = 0;
         // find ans
         for (int i = 0; i < n; i++) {
@@ -14,5 +14,21 @@ public class Solution : Relation {
             if (ans != i && (!Knows(i, ans) || Knows(ans, i))) return -1;
         }
         return ans;
+    }
+    // directed graph, in/out degress of node
+    public int FindCelebrity(int n) {
+        int[] ins = new int[n], outs = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && Knows(i, j)) {
+                    ins[j]++; outs[i]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (ins[i] == n - 1 && outs[i] == 0) return i;
+        }
+            
+        return -1;
     }
 }
