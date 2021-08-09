9913402599f77ac05bdc4d9f2802270f97993745
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-09 13:10:19 -0700

    433. Minimum Genetic Mutation BFS + DFS

--- src/433. Minimum Genetic Mutation.cs
+++ src/433. Minimum Genetic Mutation.cs
@@ -1,8 +1,8 @@
 public class Solution {
+    HashSet<string> visited = new HashSet<string>();
     // BFS, shortest path
-    public int MinMutation(string start, string end, string[] bank) {
+    public int MinMutation1(string start, string end, string[] bank) {
         if (!bank.Contains(end)) return -1;
-        var visited = new HashSet<string>();
         visited.Add(start);
         var q = new Queue<string>();
         q.Enqueue(start);
@@ -29,5 +29,26 @@ public class Solution {
             if (x[i] != y[i]) cnt++;
         }
         return cnt == 1;
+    }
+    // DFS
+    public int MinMutation(string start, string end, string[] bank) {
+        if (!bank.Contains(end)) return -1;
+        return DFS(start, end, bank);
+    }
+    int DFS(string start, string end, string[] bank) {
+        if (start == end) return 0;
+        visited.Add(start);
+        int ans = bank.Length + 1;
+        foreach (var nx in bank) {
+            if (!visited.Contains(nx) && IsMutation(start, nx)) {
+                int cnt = DFS(nx, end, bank);
+                // DFS needs to check min
+                if (cnt != -1) ans = Math.Min(ans, cnt);
+            }
+        }
+        // DFS must remove prev one
+        visited.Remove(start);
+        // add 1 for start itself
+        return ans == bank.Length + 1 ? -1 : ans + 1;
     }
 }
