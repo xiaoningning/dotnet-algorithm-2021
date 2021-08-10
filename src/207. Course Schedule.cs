b16bc89b5db904a8661d6c8eff1f84e601b3ceca
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-10 11:46:53 -0700

    Update 207. Course Schedule.cs

--- src/207. Course Schedule.cs
+++ src/207. Course Schedule.cs
@@ -1,9 +1,34 @@
 public class Solution {
+    // in/out degress of graph node
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+        int[] ins = new int[numCourses];
+        for (int c = 0; c < numCourses; c++)  {
+            prereq[c] = new List<int>();
+        }
+        // build directed graph
+        for (int i = 0; i < prerequisites.Length; i++) {
+            int pre = prerequisites[i][1];
+            int co = prerequisites[i][0];
+            prereq[pre].Add(co);
+            ins[co]++;
+        }
+        var q = new Queue<int>();
+        for (int i = 0; i < numCourses; i++)  if (ins[i] == 0) q.Enqueue(i);
+        while (q.Any()) {
+            var t = q.Dequeue();
+            foreach (var co in prereq[t]) {
+                if (--ins[co] == 0) q.Enqueue(co);
+            }
+        }
+        for (int i = 0; i < numCourses; i++)  if (ins[i] != 0) return false;
+        return true;
+    }
+    
     Dictionary<int, List<int>> prereq = new Dictionary<int, List<int>>();
     Dictionary<int, List<int>> course = new Dictionary<int, List<int>>();
     HashSet<int> taken = new HashSet<int>();
     // BFS
-     public bool CanFinish1(int numCourses, int[][] prerequisites) {
+    public bool CanFinish1(int numCourses, int[][] prerequisites) {
         int n = prerequisites.Length;
         for (int c = 0; c < numCourses; c++)  {
             prereq[c] = new List<int>();
@@ -35,7 +60,7 @@ public class Solution {
     // DFS
     int[] courseState;
     // state: 0: unknown, 1: taken, 2: checking 
-    public bool CanFinish(int numCourses, int[][] prerequisites) {
+    public bool CanFinish2(int numCourses, int[][] prerequisites) {
         int n = prerequisites.Length;
         courseState = new int[numCourses];
         for (int c = 0; c < numCourses; c++) course[c] = new List<int>();
@@ -48,7 +73,7 @@ public class Solution {
         for (int c = 0; c < numCourses; c++)  {
            if (!canDFS(c)) return false;
         }
-        //T: O(n^2)
+        // T: O(n^2)
         return true;
     }
     bool canDFS(int c) {
