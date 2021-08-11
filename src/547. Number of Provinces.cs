b7ef529514788b3dde573ff059ae7cc63019b025
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-11 14:05:08 -0700

    547. Number of Provinces DFS + UnionFind

--- src/547. Number of Provinces.cs
+++ src/547. Number of Provinces.cs
@@ -1,6 +1,29 @@
 public class Solution {
-    int[] visited;
+    // Union Find
+    int[] roots;
     public int FindCircleNum(int[][] isConnected) {
+        int n = isConnected.Length;
+        roots = new int[n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) roots[i] = i;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int ri = UnionFind(roots, i);
+                int ry = UnionFind(roots, j);
+                if (isConnected[i][j] == 1) roots[ry] = ri;
+            }
+        }
+        for (int i = 0; i < n; i++) if (roots[i] == i) ans++;
+        // T: O(n^2 * logn)
+        return ans;
+    }
+    int UnionFind(int[] roots, int x) {
+        return roots[x] == x ? x : roots[x] = UnionFind(roots, roots[x]);
+    }
+    // DFS
+    // T: O(n^2)
+    int[] visited;
+    public int FindCircleNum1(int[][] isConnected) {
         int n = isConnected.Length;
         int ans = 0;
         visited = new int[n];
