f0b4983df36438d8b9bcdae5f5765886981df894
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-12 15:11:52 -0700

    990. Satisfiability of Equality Equations

---
+++ src/990. Satisfiability of Equality Equations.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    // Union Find
+    int[] roots;
+    public bool EquationsPossible(string[] equations) {
+        // char to int
+        roots = new int[256];
+        for (int i = 0; i < 256; i++) roots[i] = i;
+        foreach (var e in equations) {
+            if (e[1] == '=') roots[UnionFind(e[0])] = roots[UnionFind(e[3])];
+        }
+        foreach (var e in equations) {
+            if (e[1] == '!' && UnionFind(e[0]) == UnionFind(e[3])) return false;
+        }
+        return true;
+    }
+    int UnionFind(int x) {
+        return roots[x] == x ? x : roots[x] = UnionFind(roots[x]);
+    }
+}
