fc9524a7f0a247e66b7597d92641195f834e9c0f
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-12 08:06:38 -0700

    1202. Smallest String With Swaps

---
+++ src/1202. Smallest String With Swaps.cs
@@ -0,0 +1,69 @@
+public class Solution {
+    // DFS
+    Dictionary<int, List<int>> g = new Dictionary<int, List<int>>();
+    int[] visited;
+    public string SmallestStringWithSwaps(string s, IList<IList<int>> pairs) {
+        int n = s.Length;
+        visited = new int[n];
+        for (int i = 0; i < n; i++) g[i] = new List<int>();
+        foreach (var p in pairs) {
+            g[p[0]].Add(p[1]);
+            g[p[1]].Add(p[0]);
+        }
+        char[] ans = new char[n];
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 1) continue;
+            var idx = new List<int>();
+            var str = new List<char>();
+            DFS(s, i, ref idx, ref str);
+            str.Sort(); idx.Sort();
+            for (int k = 0; k < idx.Count; k++) ans[idx[k]] = str[k];
+        }
+        // T: O(nlogn + k*(V+E))
+        return new string(ans);
+    }
+    void DFS(string s, int i, ref List<int> idx, ref List<char> str) {
+        if (visited[i] == 1) return;
+        visited[i] = 1;
+        str.Add(s[i]);
+        idx.Add(i);
+        foreach (int j in g[i]) DFS(s, j, ref idx, ref str); 
+    }
+    // Union Find
+    int[] roots;
+    public string SmallestStringWithSwaps1(string s, IList<IList<int>> pairs) {
+        int n = s.Length;
+        roots = new int[n];
+        for (int i = 0; i < n; i++) roots[i] = i;
+        foreach (var p in pairs) {
+            int rx = UnionFind(roots, p[0]);
+            int ry = UnionFind(roots, p[1]);
+            if(rx != ry) roots[ry] = rx;
+        }
+        var idx = new Dictionary<int, List<int>>();
+        var str = new Dictionary<int, List<char>>();
+        for (int i = 0; i < n; i++) {
+            int ri = UnionFind(roots,i);
+            if (!idx.ContainsKey(ri)) idx[ri] = new List<int>();
+            if (!str.ContainsKey(ri)) str[ri] = new List<char>();
+            idx[ri].Add(i);
+            str[ri].Add(s[i]);
+        }
+        char[] ans = new char[n];
+        int[] visited = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 1) continue;
+            int ri = UnionFind(roots, i);
+            str[ri].Sort();
+            for (int k = 0; k < idx[ri].Count; k++) {
+                ans[idx[ri][k]] = str[ri][k];
+                visited[idx[ri][k]] = 1;
+            }
+        }
+        // T: O(nlogn + V+E)
+        return new string(ans);
+    }
+    int UnionFind(int[] roots, int x) {
+        return roots[x] == x ? x : roots[x] = UnionFind(roots, roots[x]);
+    }
+}
