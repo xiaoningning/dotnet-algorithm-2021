d6a3d9dee94a9468e0049eed3928981f2a0f8a93
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-12 17:15:12 -0700

    721. Accounts Merge

---
+++ src/721. Accounts Merge.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    // OJ failed by sorting result not the same
+    // Dijkstra set: Union find
+    int[] roots;
+    public IList<IList<string>> AccountsMerge(IList<IList<string>> accounts) {
+        Dictionary<int, string> names = new Dictionary<int, string>();
+        Dictionary<string, int> ids = new Dictionary<string, int>(); 
+        Dictionary<int, List<string>> merged = new Dictionary<int, List<string>>();
+        int n = accounts.Count;
+        roots = new int[n];
+        for (int i = 0; i < n; i++) roots[i] = i;
+        for (int i = 0; i < n; i++) {
+            int len = accounts[i].Count;
+            for (int j = 1; j < len; j++) {
+               if (ids.ContainsKey(accounts[i][j])) roots[i] = UnionFind(ids[accounts[i][j]]);
+               else ids[accounts[i][j]] = UnionFind(i);
+            }
+            names[UnionFind(i)] = accounts[i][0];
+            merged[i] = new List<string>();
+        }
+        foreach (var a in accounts) {
+            for (int j = 1; j < a.Count; j++) merged[ids[a[j]]].Add(a[j]);
+        }
+        var ans = new List<IList<string>>();
+        var st = new HashSet<int>(new List<int>(roots));
+        foreach (var id in st) {
+            var t = new HashSet<string>();
+            t.Add(names[id]);
+            foreach (string e in merged[id]) t.Add(e);
+            var tl = t.ToList(); 
+            tl.Sort((x,y) => {
+                if (x.Length == y.Length) return string.Compare(x, y);
+                else return x.Length - y.Length;
+            });
+            ans.Add(tl);
+        }
+        return ans;
+    }
+    int UnionFind(int x) {
+        return roots[x] == x ? x : roots[x] = UnionFind(roots[x]); 
+    }
+}
