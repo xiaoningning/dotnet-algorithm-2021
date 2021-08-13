820c50b3735e2d045c5aedff91f9ac22c5abd3f8
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-12 21:27:07 -0700

    Update 1042. Flower Planting With No Adjacent.cs

--- src/1042. Flower Planting With No Adjacent.cs
+++ src/1042. Flower Planting With No Adjacent.cs
@@ -1,6 +1,7 @@
 public class Solution {
     // coloring graph
-    public int[] GardenNoAdj(int n, int[][] paths) {
+    // mask bit to set selected color
+    public int[] GardenNoAdj1(int n, int[][] paths) {
         int[] ans = new int[n];
         var g = new Dictionary<int, List<int>>();
         for (int i = 0; i < n; i++) g[i]  = new List<int>();
@@ -16,9 +17,28 @@ public class Solution {
             // mask all selected color by neighbors
             foreach (int j in g[i]) mask |= 1 << ans[j];
             for (int c = 1; c <= 4 && ans[i] == 0; c++) {
-                // if mask == 0, that bit is not used yet
+                // fomd the bit is 0 (not used yet)
                 if ((mask & 1 << c) == 0) ans[i] = c;
             } 
+        }
+        return ans;
+    }
+    // without mask
+    public int[] GardenNoAdj(int n, int[][] paths) {
+        int[] ans = new int[n];
+        var g = new Dictionary<int, List<int>>();
+        for (int i = 0; i < n; i++) g[i]  = new List<int>();
+        foreach (var p in paths) {
+            g[p[0] - 1].Add(p[1] - 1);
+            g[p[1] - 1].Add(p[0] - 1);
+        }
+        // path only in between two nodes
+        // no need to check DFS
+        for (int i = 0; i < n; i++) {
+            if (ans[i] != 0) continue;
+            var st = new HashSet<int>();
+            foreach (int j in g[i]) if (ans[j] != 0) st.Add(ans[j]);
+            for (int c = 1; c <= 4; c++) if (!st.Contains(c)) ans[i] = c;
         }
         return ans;
     }
