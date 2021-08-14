b0e6422f8d382449721ad8e0a2b3a9245a65ee9b
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-14 09:01:12 -0700

    Update 1263. Minimum Moves to Move a Box to Their Target Location.cs

--- src/1263. Minimum Moves to Move a Box to Their Target Location.cs
+++ src/1263. Minimum Moves to Move a Box to Their Target Location.cs
@@ -1,5 +1,6 @@
 public class Solution {
-    // TLE
+    // TLE => visited to track box and player positions
+    // Passed => visited to track box and direction
     int[] B = new int[2];
     int[] T = new int[2];
     int[] P = new int[2];
@@ -20,20 +21,23 @@ public class Solution {
         q.Enqueue(new int[]{B[0], B[1], P[0], P[1]});     
         int ans = 0;
         // track box and player position
-        var visited = new int[m, n, m, n];
+        var visited = new HashSet<(int, int, int, int)>();
         while(q.Any()) {
             int size = q.Count;
             while (--size >= 0) {
                 var c = q.Dequeue();
                 int bx = c[0], by = c[1], px = c[2], py = c[3];
                 B = new int[]{bx, by};
-                visited[bx, by, px, py] = 1;
                 for (int d = 0; d < 4; d++) {
                     int nbx = bx + dirs[d, 0], nby = by + dirs[d, 1];
+                    // player next position could push box
                     int npx = bx - dirs[d, 0], npy = by - dirs[d, 1];
                     var nx = new int[]{nbx, nby, npx, npy};
-                    if (!canPush(grid, c, nx) || visited[nbx, nby, bx, by] == 1) continue;
+                    if (!canPush(grid, c, nx) || visited.Contains((nbx,nby,dirs[d,0],dirs[d,1]))) continue;
                     if (grid[nbx][nby] == 'T') return ans + 1;
+                    visited.Add((nbx,nby,dirs[d,0],dirs[d,1]));
+                    // next position of box and player
+                    // player should stand where the box was
                     q.Enqueue(new int[]{nbx, nby, bx, by});
                 }
             }
@@ -46,14 +50,12 @@ public class Solution {
         int i = nx[0], j = nx[1];
         if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '#') return false;
         int[,] seen = new int[m,n];
-        //Console.WriteLine($"{x} - {dirx}: {y} - {diry}");
         return DFS(grid, new int[]{s[2],s[3]}, new int[]{nx[2],nx[3]}, seen);
     }
     
     bool DFS(char[][] grid, int[] s, int[] nx, int[,] seen) {
         int m = grid.Length, n = grid[0].Length;
         int x = s[0], y = s[1];
-        // Console.WriteLine($"{x}:{y} {grid[x][y]}");
         if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == '#' || seen[x, y] == 1) return false;
         if (x == B[0] && y == B[1]) return false;
         if (x == nx[0] && y == nx[1]) return true;
