9296939e5c8b16ac5eec9582493c70e2fb534acf
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-16 16:50:34 -0700

    996. Number of Squareful Arrays

---
+++ src/996. Number of Squareful Arrays.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int NumSquarefulPerms(int[] nums) {
+        int n = nums.Length;
+        int[] used = new int[n];
+        // avoid duplication
+        // perm1[i] != perm2[i]
+        Array.Sort(nums);
+        int ans = 0;
+        Action<List<int>> DFS = null;
+        DFS = (lst) => {
+            if (lst.Count == nums.Length) { ans++; return;}
+            for (int i = 0; i < n; i++) {
+                if (used[i] == 1) continue;
+                // avoid duplication
+                if (i > 0 && used[i - 1] == 0 && nums[i - 1] == nums[i]) continue;
+                if (lst.Any() && !isSquareful(lst.Last(), nums[i])) continue;
+                used[i] = 1;
+                lst.Add(nums[i]);
+                DFS(lst);
+                used[i] = 0;
+                lst.RemoveAt(lst.Count - 1);
+            }
+        };
+        DFS(new List<int>());
+        return ans;
+    }
+    bool isSquareful(int x, int y) {
+        int s = (int)Math.Sqrt(x + y);
+        return s * s == x + y;
+    }
+}
