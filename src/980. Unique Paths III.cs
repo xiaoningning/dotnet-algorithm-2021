e65089b7a4dbde0b54bdb46deb95b89bd4fabd8c
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-16 16:26:05 -0700

    Update 980. Unique Paths III.cs

--- src/980. Unique Paths III.cs
+++ src/980. Unique Paths III.cs
@@ -3,7 +3,7 @@ public class Solution {
     // T: O(4^(m*n))
     int[,] dirs = new int[4,2]{{1,0},{-1,0},{0,1},{0,-1}};
     int ans = 0;
-    public int UniquePathsIII1(int[][] grid) {
+    public int UniquePathsIII(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
         int cnt = 0;
         int sx = -1, sy = -1;
@@ -14,10 +14,28 @@ public class Solution {
                 if (grid[i][j] == 0) cnt++;
             }
         }
-        DFS(grid, sx, sy, cnt + 1);
+        
+        Action<int, int, int> DFS = null;
+        DFS = (x, y, target) => {
+            if (grid[x][y] == 2) {
+                if (target == 0) ans++; 
+            }
+            else {
+                grid[x][y] = -1;
+                int m = grid.Length, n = grid[0].Length;
+                for (int d = 0; d < 4; d++) {
+                    int i = x + dirs[d, 0], j = y + dirs[d, 1];
+                    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) continue;
+                    DFS(i, j, target - 1);
+                }
+                grid[x][y] = 0;
+            }
+        };
+        
+        DFS(sx, sy, cnt + 1);
         return ans;
     }
-    void DFS(int[][] grid, int x, int y, int target) {
+    void DFSx(int[][] grid, int x, int y, int target) {
         if (grid[x][y] == 2) {
             if (target == 0) ans++; 
             return;
@@ -27,13 +45,14 @@ public class Solution {
         for (int d = 0; d < 4; d++) {
             int i = x + dirs[d, 0], j = y + dirs[d, 1];
             if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) continue;
-            DFS(grid, i, j, target - 1);
+            DFSx(grid, i, j, target - 1);
         }
         grid[x][y] = 0;
     }
+    
     // DP + bit mask + inline c# func
     // T: O(m*n* 2^(m* n)) is much slower than DFS
-    public int UniquePathsIII(int[][] grid) {
+    public int UniquePathsIII2(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
         int sx = -1, sy = -1;
         // (i, j, state) => # of path
