88c027a1b0e2b10668c752a6458ea43185f7acec
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-17 11:09:02 -0700

    Update 303. Range Sum Query - Immutable.cs

--- src/303. Range Sum Query - Immutable.cs
+++ src/303. Range Sum Query - Immutable.cs
@@ -1,5 +1,5 @@
 public class NumArray1 {
-    int[] dp; // prefix sum of nums;
+    int[] dp; // Cumulative sum of nums;
     public NumArray1(int[] nums) {
         dp = (int[])nums.Clone();
         for (int i = 1; i < nums.Length; i++) dp[i] += dp[i-1];
@@ -12,7 +12,7 @@ public class NumArray1 {
 }
 // resize dp to avoid left == 0 for inclusive sum
 public class NumArray {
-    int[] dp; // prefix sum of nums;
+    int[] dp; // Cumulative sum of nums;
     public NumArray(int[] nums) {
         int n = nums.Length;
         dp = new int[n + 1];
