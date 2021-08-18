912c7869194df5e822d3db4c83e74af26077148e
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-17 23:52:27 -0700

    901. Online Stock Span monotonic stack + dp

--- src/901. Online Stock Span.cs
+++ src/901. Online Stock Span.cs
@@ -1,8 +1,8 @@
 // Monotonic stack decreasing
-public class StockSpanner {
+public class StockSpanner1 {
     // (price, span/cnt)
     Stack<(int, int)> q;
-    public StockSpanner() { q = new Stack<(int, int)>(); }
+    public StockSpanner1() { q = new Stack<(int, int)>(); }
     // Next() T: O(1) stack operation is O(1)
     public int Next(int price) {
         int cnt = 1;
@@ -13,7 +13,28 @@ public class StockSpanner {
         return cnt;
     }
 }
-
+// DP
+public class StockSpanner {
+    List<int> prices = new List<int>();
+    List<int> dp = new List<int>(); // span/cnt of price(i)
+    public StockSpanner() {}
+    // Next() T: O(n) scan dp
+    public int Next(int price) {
+        if (!prices.Any() || price < prices.Last()) dp.Add(1);
+        else {
+            // start from the last one
+            int j = prices.Count - 1;
+            // next to check is the larger than the last one.
+            while (j >= 0 && price >= prices[j]) j -= dp[j];
+            // j is the idx of price larger than the current one
+            // prices.Count - j is the cnt/span of previous consecutive days 
+            // of smaller prices
+            dp.Add(prices.Count - j);
+        }
+        prices.Add(price);
+        return dp.Last();
+    }
+}
 /**
  * Your StockSpanner object will be instantiated and called as such:
  * StockSpanner obj = new StockSpanner();
