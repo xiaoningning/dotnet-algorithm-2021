8835c260ede8137f103d13f846c325674fce0d28
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-17 21:26:09 -0700

    Update 769. Max Chunks To Make Sorted.cs

--- src/769. Max Chunks To Make Sorted.cs
+++ src/769. Max Chunks To Make Sorted.cs
@@ -1,5 +1,23 @@
 public class Solution {
+    // monotonic stack
+    // T: O(n * logn)
     public int MaxChunksToSorted(int[] arr) {
+        int n = arr.Length;
+        var st = new HashSet<int>();
+        for (int i = 0; i < n; i++) {
+            if (!st.Any() || st.Last() < arr[i]) st.Add(arr[i]);
+            else {
+                int curMx = st.Last();
+                st.Remove(curMx);
+                // remove all st.Last() > arr[i] since they must be in one chunk
+                while (st.Any() && st.Last() > arr[i]) st.Remove(st.Last());
+                st.Add(curMx);
+            }
+        }
+        return st.Count;
+    }
+    // T: O(n)
+    public int MaxChunksToSorted1(int[] arr) {
         int n = arr.Length;
         int ans = 0;
         int mx = Int32.MinValue;
