f459089d02ca0829578b9fb471f0a556a562a35b
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-17 18:24:21 -0700

    239. Sliding Window Maximum Monotonic queue/stack

---
+++ src/239. Sliding Window Maximum.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    // Monotonic Queue
+    // T: O(n)
+    public int[] MaxSlidingWindow1(int[] nums, int k) {
+        var ans = new List<int>();
+        var q = new List<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (q.Any() && q.First() + k <= i) q.RemoveAt(0);
+            // keep the same max value in the monotonic queue
+            while (q.Any() && nums[q.Last()] < nums[i]) q.RemoveAt(q.Count - 1);
+            q.Add(i);
+            // assume k is always invalid
+            if (i >= k - 1) ans.Add(nums[q.First()]);
+        }
+        return ans.ToArray();
+    }
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+        var mq = new MonotonicQueue();
+        var ans = new List<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            mq.Push(nums[i]);
+            if (i >= k - 1) {
+                ans.Add(mq.Max());
+                if (nums[i - (k - 1)] == mq.Max()) mq.Pop();
+            }
+        }
+        return ans.ToArray();
+    }
+}
+// value decreasing monotonic queue
+public class MonotonicQueue {
+    List<int> q;
+    public MonotonicQueue() { q = new List<int>();}
+    public void Push(int e) {
+        // "<" since remove 1st of q based on max value
+        while (q.Any() && q.Last() < e) q.RemoveAt(q.Count - 1);
+        q.Add(e);
+    }
+    public void Pop() { q.RemoveAt(0); }
+    public int Max() { return q.First();}
+}
