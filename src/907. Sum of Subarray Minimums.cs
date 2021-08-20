eac39b3487e03418cc2bdcb20455e4dae4b36be7
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-20 12:44:17 -0700

    Update 907. Sum of Subarray Minimums.cs

--- src/907. Sum of Subarray Minimums.cs
+++ src/907. Sum of Subarray Minimums.cs
@@ -26,7 +26,7 @@ public class Solution {
     For 2, the left and right boudray is: ... | 2 4 |
     For 4, the left and right boudary is: ... | 4 |
     */
-    public int SumSubarrayMins(int[] arr) {
+    public int SumSubarrayMins2(int[] arr) {
         int M = (int)1e9 + 7, ans = 0, n = arr.Length;
         int[] left = new int[n], right = new int[n];
         // (num, len)
@@ -51,6 +51,29 @@ public class Solution {
             st.Push((arr[i], len));
             right[i] = len;
         }
+        for (int i = 0; i < n; i++){
+            ans = (int)(ans + (long)arr[i] * left[i] * right[i] % M) % M;
+        }
+        return ans;
+    }
+    // V2: cleaner code
+    public int SumSubarrayMins(int[] arr) {
+        int M = (int)1e9 + 7, ans = 0, n = arr.Length;
+        int[] left = new int[n], right = new int[n];
+        Array.Fill(left, 1); Array.Fill(right, 1);
+        
+        Action<int[], int, int, int> FindLens = (lens, start, end, step) => {
+            var st = new Stack<(int,int)>();   // (num, len)
+            int i = start;
+            while (i != end) {
+                int cur = step == -1 ? arr[i] - 1 : arr[i];
+                while (st.Any() && st.Peek().Item1 > cur) lens[i] += st.Pop().Item2;
+                st.Push((arr[i], lens[i]));
+                i += step;
+            }
+        };
+        FindLens(left, 0, n, 1);
+        FindLens(right, n - 1, -1, -1);
         for (int i = 0; i < n; i++){
             ans = (int)(ans + (long)arr[i] * left[i] * right[i] % M) % M;
         }
