a836c46434942cb9e5cce972a6c3190be14b356a
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-20 13:18:12 -0700

    1019. Next Greater Node In Linked List

---
+++ src/1019. Next Greater Node In Linked List.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    // Monotonic stack
+    // O(n)
+    public int[] NextLargerNodes1(ListNode head) {
+        int n = 0;
+        var p = head;
+        while ( p != null) { n++; p = p.next; }
+        int[] ans = new int[n];
+        var st = new Stack<(int,int)>();
+        int i = 0;
+        p = head;
+        while (p != null) {
+            while (st.Any() && p.val > st.Peek().Item1) {
+                var pre = st.Pop();
+                ans[pre.Item2] = p.val;
+            }
+            st.Push((p.val, i));
+            p = p.next;
+            i++;
+        }
+        while (st.Any()) ans[st.Pop().Item2] = 0;
+        return ans;
+    }
+    public int[] NextLargerNodes(ListNode head) {
+        var nums = new List<int>();
+        var p = head;
+        while (p != null) { nums.Add(p.val); p = p.next; }
+        int[] ans = new int[nums.Count];
+        var st = new Stack<int>();
+        for (int i = nums.Count - 1; i >= 0; --i) {
+            // >= since 0 case is handle here.  
+            // it needs to pop all, include itself
+            while (st.Any() && nums[i] >= st.Peek()) st.Pop();
+            ans[i] = st.Any() ? st.Peek() : 0;
+            st.Push(nums[i]);
+        }
+        return ans;
+    }
+}
