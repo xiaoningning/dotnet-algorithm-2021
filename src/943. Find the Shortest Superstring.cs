c4ab4c8de6a540450d0d52afc1fc2ac549d8f8fd
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-23 19:27:14 -0700

    Update 943. Find the Shortest Superstring.cs

--- src/943. Find the Shortest Superstring.cs
+++ src/943. Find the Shortest Superstring.cs
@@ -1,4 +1,40 @@
 public class Solution {
+    // DFS: TLE
+    // ans is not correct
+    public string ShortestSuperstring2(string[] words) {
+        int n = words.Length;
+        int[,] overlap = new int[n,n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                string A = words[i], B = words[j];
+                 if (i == j) continue;
+                int len = Math.Min(A.Length, B.Length);
+                for (; len > 0; len--) {
+                    if (A.Substring(A.Length - len) == B.Substring(0, len)) {
+                        overlap[i, j] = len;
+                        break;
+                    }
+                }
+            }
+        }
+        int mn = Int32.MaxValue;
+        string ans = "";
+        int[] used = new int[n];
+        Action<int, string> DFS = null;
+        DFS = (start, cur) => {
+            if (cur.Length >= mn) return;
+            if (start == n) { mn = cur.Length; ans = cur; return; }
+            for (int i = 0; i < n; i++) {
+                if (used[i] == 1) continue;
+                used[i] = 1;
+                string tmp = cur;
+                string nx = cur + (start == 0 || overlap[start - 1, i] == 0 ? words[i] : words[i].Substring(0, overlap[start - 1, i]));
+                DFS(start + 1, nx);
+            }
+        };
+        DFS(0,"");
+        return ans;
+    }
     // Travelling salesman problem (TSP) + Hamiltonian path
     // a shortest path of weighted graph
     // Time complexity: O(n^2 * 2^n)
@@ -25,7 +61,7 @@ public class Solution {
         // 1: visited, 0: not visited
         string[,] dp = new string[1 << n, n];
         for (int i = 0; i < n; i++) dp[1 << i, i] = words[i];
-        for (int mask = 1; mask < (1 << n); mask++) {
+        for (int mask = 0; mask < (1 << n); mask++) {
             // visit j from i
             for (int j = 0; j < n; j++) {
                 // j is not in mask
