5cd9b38a272d356c2d22afea46ad9a6dde8f5ced
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-30 13:28:33 -0700

    62. Unique Paths DP + recursion

---
+++ src/62. Unique Paths.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    // DP
+    public int UniquePaths1(int m, int n) {
+        int[,] dp = new int[m,n];
+        dp[0,0] = 1;
+        for (int i = 0; i < n; i++) dp[0,i] = 1;
+        for (int i = 0; i < m; i++) dp[i,0] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++)
+                dp[i,j] = dp[i-1,j] + dp[i, j-1];
+        }
+        return dp[m-1,n-1];
+    }
+    // recursion + memo
+    public int UniquePaths(int m, int n) {
+        int[,] memo = new int[m,n];
+        Func<int, int, int> f = null;
+        f = (i,j) => {
+            if (i == 0 || j == 0) return 1;
+            if (memo[i,j] > 0) return memo[i,j];  
+            int leftPaths = f(i - 1, j);
+            int topPaths = f(i, j - 1);
+            return memo[i,j] = leftPaths + topPaths;
+        };
+        return f(m-1,n-1);
+    }
+}
