d6bae67b21e1742bbc48e3061251f1faf23c5882
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-31 00:23:02 -0700

    Update 1105. Filling Bookcase Shelves.cs

--- src/1105. Filling Bookcase Shelves.cs
+++ src/1105. Filling Bookcase Shelves.cs
@@ -1,23 +1,24 @@
 public class Solution {
     // DP T: O(n^2)
-    public int MinHeightShelves1(int[][] books, int shelfWidth) {
+    public int MinHeightShelves(int[][] books, int shelfWidth) {
         int n = books.Length;
         // min height of book i
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             int w = books[i - 1][0], h = books[i - 1][1];
+            // worst case for book[i - 1]: start a new level
             dp[i] = dp[i - 1] + h;
-            // add previous book into the same level to min total height
-            for (int j = i - 1 - 1; j >= 0 && w + books[j][0] <= shelfWidth; j--) {
-                w += books[j][0];
-                h = Math.Max(h, books[j][1]);
-                dp[i] = Math.Min(dp[i], dp[j] + h);
+            // add previous book into the i level to min total height
+            for (int j = i - 1; j >= 1; j--) {
+                if ((w += books[j - 1][0]) > shelfWidth) break;
+                h = Math.Max(h, books[j - 1][1]);
+                dp[i] = Math.Min(dp[i], dp[j - 1] + h);
             }
         } 
         return dp[n];
     }
     // DP v2
-    public int MinHeightShelves(int[][] books, int shelfWidth) {
+    public int MinHeightShelves2(int[][] books, int shelfWidth) {
         int n = books.Length;
         // min height of book i
         int[] dp = new int[n];
