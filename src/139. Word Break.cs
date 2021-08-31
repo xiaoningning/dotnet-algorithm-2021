1e0be8551509a5921babab11d2682ae752117b04
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-08-30 21:45:22 -0700

    139. Word Break DP (DFS + Memo) + BFS

---
+++ src/139. Word Break.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    // DP => DFS + memo
+    // T: O(n^2)
+    // S: O(n^2)
+    public bool WordBreak1(string s, IList<string> wordDict) {
+        var memo = new Dictionary<string, bool>();
+        Func<string, bool> DFS = null;
+        DFS = (str) => {
+            if (str.Length == 0) return true;
+            if (memo.ContainsKey(str)) return memo[str];
+            if (wordDict.Contains(str)) return memo[str] = true;
+            for (int len = 1; len <= str.Length; len++) {
+                if (wordDict.Contains(str.Substring(0,len)) 
+                    && DFS(str.Substring(len))) return memo[str] = true;
+            }
+            return memo[str] = false;
+        };
+        return DFS(s);
+    }
+    // BFS no memo
+    public bool WordBreak(string s, IList<string> wordDict) {
+        var dict = new HashSet<string>(wordDict);
+        var visited = new int[s.Length];
+        var q = new Queue<int>();
+        q.Enqueue(0);
+        while (q.Any()) {
+            int start = q.Dequeue();
+            if (visited[start] == 1) continue;
+            for (int i = start + 1; i <= s.Length; i++) {
+                if (dict.Contains(s.Substring(start, i - start))) {
+                    q.Enqueue(i);
+                    if (i == s.Length) return true;
+                }
+                visited[start] = 1;
+            }
+        }
+        return false;
+    }
+}
