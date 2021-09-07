5217856111868e49e7d94cc3af7b387d52afa874
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-06 22:42:28 -0700

    Update 1277. Count Square Submatrices with All Ones.cs

--- src/1277. Count Square Submatrices with All Ones.cs
+++ src/1277. Count Square Submatrices with All Ones.cs
@@ -3,11 +3,11 @@ public class Solution {
     // T: O(m*n) S: O(1)
     public int CountSquares(int[][] matrix) {
         int ans = 0, m = matrix.Length, n = matrix[0].Length;
+        // matrix track # of square at (i,j)
         for (int i = 0; i < m; i++) 
             for (int j = 0; j < n; j++) {
                 if (i > 0 && j > 0 && matrix[i][j] == 1) 
                     matrix[i][j] = new int[]{matrix[i-1][j-1],matrix[i-1][j],matrix[i][j-1]}.Min() + 1;
-                // # of sqaures = sum(dp[i])
                 ans += matrix[i][j];
             }
         return ans;
