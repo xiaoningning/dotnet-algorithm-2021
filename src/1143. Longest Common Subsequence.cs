9bd4f16b088250fcb0f61ecc4a670875fa4a7c2e
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-09 18:27:31 -0700

    1143. Longest Common Subsequence DP + recursion + memo

--- src/1143. Longest Common Subsequence.cs
+++ src/1143. Longest Common Subsequence.cs
@@ -1,6 +1,6 @@
 public class Solution {
     // recursion + memo
-    public int LongestCommonSubsequence(string text1, string text2) {
+    public int LongestCommonSubsequence1(string text1, string text2) {
         int l1 = text1.Length, l2 = text2.Length;
         int[,] memo = new int[l1,l2];
         for (int i = 0; i < l1; i++) for (int j = 0; j < l2; j++) memo[i,j] = -1;
@@ -11,5 +11,14 @@ public class Solution {
             return memo[i,j] = text1[i] == text2[j] ? 1 + DFS(i+1,j+1) : Math.Max(DFS(i+1,j), DFS(i, j+1)); 
         };
         return DFS(0,0);
+    }
+    // DP
+    public int LongestCommonSubsequence(string text1, string text2) {
+        int l1 = text1.Length, l2 = text2.Length;
+        int[,] dp = new int[l1+1,l2+1];
+        for (int i = 1; i <= l1; i++) 
+            for (int j = 1; j <= l2; j++) 
+                dp[i,j] = text1[i-1] == text2[j-1] ? dp[i-1,j-1] + 1 : Math.Max(dp[i, j-1], dp[i-1,j]);
+        return dp[l1,l2];
     }
 }
