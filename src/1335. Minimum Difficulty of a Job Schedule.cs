78898c2a589fd8617e9fe06b380426efb5907eb8
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-13 23:17:51 -0700

    Update 1335. Minimum Difficulty of a Job Schedule.cs

--- src/1335. Minimum Difficulty of a Job Schedule.cs
+++ src/1335. Minimum Difficulty of a Job Schedule.cs
@@ -39,8 +39,8 @@ public class Solution {
         for (int i = 0; i <= n; i++) 
             for (int j = 0; j <= d; j++) dp[i,j] = jobDifficulty.Sum() + 1;
         dp[0,0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int k = 1; k <= d; k++) {
+        for (int k = 1; k <= d; k++) {
+             for (int i = 1; i <= n; i++) {
                 // each day at least one job
                 for (int j = i - 1, mx = 0; j >= k - 1; j--) {
                     mx = Math.Max(mx, jobDifficulty[j]);
