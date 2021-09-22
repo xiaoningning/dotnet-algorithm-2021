0d304e4c5a1a4c0575b1fcf8eae7bb7ad744a31f
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-21 21:43:35 -0700

    1872. Stone Game VIII DP

---
+++ src/1872. Stone Game VIII.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    // Naive DP (min-max) takes O(n2) which leads to TLE.
+    // The key of this problem is that each player takes k stones, 
+    // but put their sum back as a new stone, 
+    // so you can assume all the original stones are still there, 
+    // but opponent has to start from the k+1 th stone.
+    // dp[i] = sum[0,i] - max(dp[i+1], dp[i+2], dp[n-1]);
+    // T: O(n) S: O(1)
+    public int StoneGameVIII(int[] stones) {
+        int n = stones.Length;
+        for (int i = 1; i < n; i++) stones[i] += stones[i-1];
+        // compute dp in reverse order
+        // last one is to take the whole array
+        int mx = stones.Last();
+        for (int i = n - 2; i >= 1; i--)
+            mx = Math.Max(mx, stones[i] - mx);
+        return mx;
+    }
+}
