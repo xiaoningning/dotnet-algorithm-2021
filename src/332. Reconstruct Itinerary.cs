efb565c2118db33df78f38fc4a59d5d06b01e89f
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-24 14:35:36 -0700

    332. Reconstruct Itinerary DFS + Stack

--- src/332. Reconstruct Itinerary.cs
+++ src/332. Reconstruct Itinerary.cs
@@ -1,7 +1,8 @@
 public class Solution {
     Dictionary<string, List<string>> g = new Dictionary<string, List<string>>();
     List<string> ans = new List<string>();
-    public IList<string> FindItinerary(IList<IList<string>> tickets) {
+    // DFS
+    public IList<string> FindItinerary1(IList<IList<string>> tickets) {
         foreach (var t in tickets) {
             if (!g.ContainsKey(t[0])) g[t[0]] = new List<string>();
             if (!g.ContainsKey(t[1])) g[t[1]] = new List<string>();
@@ -19,5 +20,26 @@ public class Solution {
             DFS(nx);
         }
         ans.Add(s);
+    }
+    // stack
+    public IList<string> FindItinerary(IList<IList<string>> tickets) {
+        foreach (var t in tickets) {
+            if (!g.ContainsKey(t[0])) g[t[0]] = new List<string>();
+            if (!g.ContainsKey(t[1])) g[t[1]] = new List<string>();
+            g[t[0]].Add(t[1]);
+        }
+        foreach (var kv in g) kv.Value.Sort();
+        var st = new Stack<string>();
+        st.Push("JFK");
+        while (st.Any()) {
+            var t = st.Peek();
+            if (!g[t].Any()) ans.Insert(0, st.Pop());
+            else {
+                string nx = g[t].First();
+                st.Push(nx);
+                g[t].Remove(nx);
+            }
+        }
+        return ans;
     }
 }
