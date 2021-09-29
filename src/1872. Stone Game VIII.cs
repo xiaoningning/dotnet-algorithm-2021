d198b3cc51ff47d31215d28a25e2e19714651ed8
XiaoNing <1930967+xiaoningning@users.noreply.github.com>
2021-09-29 11:39:23 -0700

    Update 1872. Stone Game VIII.cs

--- src/1872. Stone Game VIII.cs
+++ src/1872. Stone Game VIII.cs
@@ -3,7 +3,8 @@ public class Solution {
     // The key of this problem is that each player takes k stones, 
     // but put their sum back as a new stone, 
     // so you can assume all the original stones are still there, 
-    // but opponent has to start from the k+1 th stone.
+    // but the 2nd player has to start from the k+1 th stone 
+    // and minimize 1st player score.
     // dp[i] := max # of difference by two players
     // dp[i] = sum[0,i] - max(dp[i+1], dp[i+2], dp[n-1]);
     // T: O(n) S: O(1)
@@ -13,11 +14,11 @@ public class Solution {
         for (int i = 1; i < n; i++) stones[i] += stones[i-1];
         // compute dp in reverse order
         // last one is to take the whole array
-        // dp[N - 2] = sum[N - 1]
+        // dp[N - 1] = sum[N - 1]
         int mx = stones.Last(); 
         // x > 1 => i >= 1
         for (int i = n - 2; i >= 1; i--)
-            // dp[i - 1] = max(dp[i], sum[i] - dp[i]) 
+            // dp[i] = max(dp[i+1], sum[i] - dp[i+1]) 
             mx = Math.Max(mx, stones[i] - mx);
         return mx;
     }
